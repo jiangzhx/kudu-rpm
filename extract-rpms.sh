0b7d7e2c674c77bb94cb8e9b2c118bdfef4a2867
zhenxing jiang <jiangzhx@gmail.com>
2023-04-12 18:37:14 +0800

    Update extract-rpms.sh

--- extract-rpms.sh
+++ extract-rpms.sh
@@ -5,7 +5,7 @@ docker buildx build --load --platform linux/arm64 . -t kudu-rpm
 KUDU_VERSION=1.15.0
 RELEASE=1
 
-KUDU_RPM=kudu-${KUDU_VERSION}-${RELEASE}.x86_64.rpm
+KUDU_RPM=kudu-${KUDU_VERSION}-${RELEASE}.arm64.rpm
 
 IMG_ID=$(dd if=/dev/urandom bs=1k count=1 2> /dev/null | LC_CTYPE=C tr -cd "a-z0-9" | cut -c 1-22)
 docker create --name ${IMG_ID} kudu-rpm
